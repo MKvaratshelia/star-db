74540f37ffa0f421678c1b49e5ccfd9584792ec4
Kvaratshelia Mihail <kvaratsheliamihail@yandex.ru>
2020-12-17 15:11:20 +0300

    Подготовил компоненты и сделал стили

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,26 @@
+import React from "react";
+import Header from "../components/header/Header";
+import RandomPlanet from "../components/randomPlanet/RandomPlanet";
+import ItemList from "../components/itemList/ItemList";
+import PersonDetails from "../components/personDetails/PersonDetails";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Header />
+      <RandomPlanet />
+
+      <div className="row mb2">
+        <div className="col-md-6">
+          <ItemList />
+        </div>
+        <div className="col-md-6">
+          <PersonDetails />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
