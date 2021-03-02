019aff4bc22ad7c5c84a6b7331d9564db2a87f06
Kvaratshelia Mihail <kvaratsheliamihail@yandex.ru>
2021-03-02 23:11:19 +0300

    добавил обработку несуществующих путей

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "../components/header/Header";
 import RandomPlanet from "../components/randomPlanet/RandomPlanet";
 import SwapiService from "../services";
@@ -6,7 +7,6 @@ import "./App.css";
 import { SwapiServiceProvider } from "./swapiServiceContext/swapiServiceContext";
 import ErrorBoundry from "./errorBoundry/ErrorBoundry";
 import { PeoplePage, PlanetPage, StarshipsPage } from "./pages/index";
-import { BrowserRouter, Route } from "react-router-dom";
 import { StarshipDetails } from "./sw-components";
 
 class App extends Component {
@@ -20,18 +20,21 @@ class App extends Component {
             <div>
               <Header />
               <RandomPlanet />
-              <Route path="/" render={() => <h2>Welcome StarDB</h2>} exact />
-              <Route path="/people/:id?" component={PeoplePage} />
-              <Route path="/planets" component={PlanetPage} />
-              <Route path="/starships" exact component={StarshipsPage} />
-              <Route
-                path="/starships/:id"
-                render={({ match }) => {
-                  // получает 3 параметра match, history, location
-                  const { id } = match.params;
-                  return <StarshipDetails itemId={id} />;
-                }}
-              />
+              <Switch>
+                <Route path="/" render={() => <h2>Welcome StarDB</h2>} exact />
+                <Route path="/people/:id?" component={PeoplePage} />
+                <Route path="/planets" component={PlanetPage} />
+                <Route path="/starships" exact component={StarshipsPage} />
+                <Route
+                  path="/starships/:id"
+                  render={({ match }) => {
+                    // получает 3 параметра match, history, location
+                    const { id } = match.params;
+                    return <StarshipDetails itemId={id} />;
+                  }}
+                />
+                <Route render={() => <h2>Page not found</h2>} />
+              </Switch>
             </div>
           </BrowserRouter>
         </SwapiServiceProvider>
